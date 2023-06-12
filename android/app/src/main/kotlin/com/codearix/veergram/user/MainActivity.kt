a35473a6473d661e22a8d53a5b6a5f30b327d3e2
Hazrat Sharif <50118391+hazratsahrif@users.noreply.github.com>
2023-06-12 23:48:31 +0500

    User app completed

--- android/app/src/main/kotlin/com/codearix/veergram/user/MainActivity.kt
+++ android/app/src/main/kotlin/com/codearix/veergram/user/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.iqonic.servicebooking
+package com.codearix.veergram.user
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
